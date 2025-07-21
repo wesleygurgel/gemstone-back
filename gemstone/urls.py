3582bb8a6e230e4978622c0a6ae304a13e5d50b5
Wesley Gurgel <wesleygurgel27@gmail.com>
2025-07-21 10:10:45 -0300

    fix: urls for media files

--- gemstone/urls.py
+++ gemstone/urls.py
@@ -33,5 +33,4 @@ urlpatterns = [
 ]
 
 # Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
